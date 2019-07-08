d28b1a3c5ea377727c27098888c042f65024344c
Stuart Barry <stuart.d.barry@gmail.com>
2019-07-08 15:43:09 +0100

    3: Refactor GameController to be winforms specific

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -22,32 +22,7 @@ namespace Tetris
             var gamePanel = form.GamePanel;
             var grid = new GameGrid(gamePanel.Width / 20, gamePanel.Height / 20);
             var renderer = new PanelRenderer(form.GamePanel, grid, 20, 20);
-
-            var gameController = new GameController(grid);
-            form.KeyDown += (sender, e) =>
-                {
-                    Direction key;
-                    switch (e.KeyCode)
-                    {
-                        case Keys.Left:
-                            key = Direction.Left;
-                            break;
-                        case Keys.Right:
-                            key = Direction.Right;
-                            break;
-                        case Keys.Up:
-                            key = Direction.Up;
-                            break;
-                        case Keys.Down:
-                            key = Direction.Down;
-                            break;
-                        default:
-                            return;
-                            break;
-                    }
-                    gameController.KeyPressed(key);
-                };
-
+            var gameController = new WinFormsGameController(form, grid);
 
             Timer ticky;
             ticky = new Timer();
